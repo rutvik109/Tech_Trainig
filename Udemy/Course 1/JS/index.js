b55b61e7d279041a018f63d8dfd56a0128b2639a
Rutvik <rutvikpatel1092@gmail.com>
2023-02-03 05:19:10 -0800

    Udemy Course Practice

---
+++ Udemy/Course 1/JS/index.js
@@ -0,0 +1,93 @@
+// alert('This is alert message');
+
+// document.write("this is used to write text on screen");
+
+// var a = 0;
+
+// console.log("value of a = "+ a);
+// a++;
+// console.log("value of a = "+ a);
+
+
+// var b = "rutvik";
+// var c = false;
+
+// console.log(b);
+
+
+
+// var a = 0;
+
+// myFunction(a,10,100);
+// console.log(c);
+
+// function myFunction(a,b,c){
+//      b++;
+//      c  = "Hello";
+//      console.log(a," ",b," ",c);
+// }
+
+// var d = 0;
+
+// function myFunction(a,b,c){
+//      d++;
+//      a++;
+//      console.log("a = "+a+"b = "+b+"c = "+c+"d = "+d);
+// }
+
+
+
+// var z = '2';
+
+// if(z === 2){
+//     console.log("it is integer");
+// }
+// else {
+//     console.log("it is string");
+// }
+
+
+
+// var  y = 3;
+
+// switch(y){
+//     case 1:
+//         console.log("Value of y is : "+ y);
+//     break;
+//     case 3:
+//         console.log("Value of y is : "+ y);
+//     break;
+//     default:
+//         console.log("NO condition match");
+//     break;
+// }
+
+
+
+// var k = 5;
+// var l = (k == 6? "yes" : "no");
+// console.log(l);
+
+
+// var a = 0;
+//  while(a < 5){
+//     console.log("Value of a = "+ a);
+//     a++;
+//  }
+
+//  for(b = 0; b < 5;b++){
+//     console.log("Value of b = "+ b)
+//  }
+
+
+
+// var a = Math.floor(Math.random() * 1000);
+// console.log(a);
+
+
+
+// var arr = [12,"Name"];
+// console.log(arr[1]);    
+
+// var obj = {name: "Rutvik",std: 10};
+// console.log(obj.std);
