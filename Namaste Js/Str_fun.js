fd59a9a3937dc1d4d4ac512a4b4564fc5e0adcf3
Rutvik <rutvikpatel1092@gmail.com>
2023-02-05 09:28:21 -0800

    str functions added

---
+++ Namaste Js/Str_fun.js
@@ -0,0 +1,32 @@
+let text1 = "apple, banaNa, Kiwi";
+let text2 = "my name is rutvik";
+
+console.log(text1.length);
+
+console.log(text1.slice(0,6));
+
+console.log(text1.substring(0,6));
+
+console.log(text1.substr(2,6))
+
+console.log(text1.charAt(1));
+
+console.log(text1.concat(" "+text2));
+
+console.log(text1.replace("a","b"));
+
+console.log(text1.replace(/a/g,"b"));
+
+console.log(text1.replaceAll("a","b"));
+
+console.log(text1.indexOf("b"));
+
+console.log(text1.toLowerCase());
+
+console.log(text1.toUpperCase());
+
+
+
+
+
+
---
+++ Namaste Js/exercise.js
@@ -0,0 +1,7 @@
+const numbers = [4, 9, 16, 25, 29];
+
+console.log(numbers.find((x)=>{
+    if(x ==  16){
+        return x;
+    }
+}));
