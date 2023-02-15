db83d315cc766fe70a877ce9532683be54c756aa
Rutvik <rutvikpatel1092@gmail.com>
2023-02-14 16:32:21 -0800

    reactive form upto formBuilder

--- Angular - Training/Angular_Youtube_Reactive/src/app/app.component.ts
+++ Angular - Training/Angular_Youtube_Reactive/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular_Youtube_Reactive';
+
+  constructor(private fb : FormBuilder){};
+
+   registrationForm = this.fb.group({
+      userName: ["rutvik"],
+      password: [""],
+      confirmPassword: [""],
+      address: this.fb.group({
+        city: [""],
+        state: [""],
+        postCode: [""]
+      })
+   })
+
+//     registrationForm = new FormGroup(
+//       {
+//         userName: new FormControl(""),
+//         password: new FormControl(""),
+//         confirmPassword: new FormControl(""),
+//         address: new FormGroup({
+//           city: new FormControl(""),
+//           state: new FormControl(""),
+//           postCode: new FormControl("")
+//         })
+//       }
+//     )
+
+    OnLoad(){
+      this.registrationForm.setValue({
+        userName: "rutvik",
+        password: "test",
+        confirmPassword: "test",
+        address: {
+          city: "Ahmedabad",
+          state: "gujrat",
+          postCode: "380026",
+
+      }
+    })
+}
 }
