39b93078e38c19e11b0c903cda7e5dc82cea93ec
Rutvik <rutvikpatel1092@gmail.com>
2023-02-17 18:34:26 -0800

    getting all the users data

--- Angular-Restro/src/app/login-restro/login-restro.component.ts
+++ Angular-Restro/src/app/login-restro/login-restro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { RestoService } from '../resto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-restro',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-restro.component.css']
 })
 export class LoginRestroComponent {
+
+constructor(private fb : FormBuilder, private restoService : RestoService, private router : Router ){}
+
+loginResto = this.fb.group({
+  email: [''],
+  password: ['']
+  })
+
+OnLogin(){
+  this.restoService.loginUser().subscribe((data) => {
+   console.log(data);
+  });
+}
+
 
 }
