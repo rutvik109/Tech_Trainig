37ce2b88555980a0884c5629247c0d2eead0a23c
Rutvik <rutvikpatel1092@gmail.com>
2023-02-23 17:11:03 -0800

    docheck

--- role-based/src/app/app.component.ts
+++ role-based/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'role-based';
+  ismenurequired = false;
+
+  constructor(private router : Router){};
+
+  ngDoCheck():void{
+      let currenturl = this.router.url;
+      if(currenturl === "/login" || currenturl === "/register"){
+        this.ismenurequired = false;
+      }
+      else{
+        this.ismenurequired = true;
+      }
+  }
 }
